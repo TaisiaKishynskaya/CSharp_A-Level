b62f0d70448f966cd11d9b20ac725f9cfa9c8040
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-08-11 17:07:44 +0300

    Copy the solution from the slides "Creating library"

---
+++ Mod1.Lesson1.Hw1/ShowCase/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UtilityLibraries;
+
+namespace ShowCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var row = 0;
+
+            do
+            {
+                if (row == 0 || row >= 25)
+                {
+                    ResetConsole();
+                }
+
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) 
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Input: {input}");
+                Console.WriteLine("Begins with upper? " + 
+                    $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+                Console.WriteLine();
+                row += 4;
+            }
+            while (true);
+            return;
+
+            void ResetConsole() 
+            {
+                if (row > 0)
+                {
+                    Console.WriteLine("Press and key to continue...");
+                    Console.ReadKey();
+                }
+
+                Console.Clear();
+                Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit;" +
+                    $" otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+                row = 3;
+            }
+        }
+    }
+}
