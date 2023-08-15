54468a9160f6300566668cdb83356829d734de1f
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-08-15 18:32:24 +0300

    Done HW 1.2, task 2 (A-Level)

---
+++ Mod1.Lection2.Hw2.Task2/Mod1.Lection2.Hw2.Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿var arrA = new int[20];
+var arrB = new int[20];
+
+FillingArrays();
+Console.WriteLine($"Sorted array {string.Join(",", BubbleSortArr(arrB))}");
+
+void FillingArrays()
+{
+    var random = new Random();
+
+    for (var i = 0; i < arrA.Length; i++)
+    {
+        arrA[i] = random.Next(0, 1000);
+        Console.WriteLine($"Arr A, element {i}: {arrA[i]}");
+
+        var arrElement = arrA[i] <= 888 ? arrB[i] = arrA[i] : arrA[i] = random.Next(0, 888);
+        Console.WriteLine($"Arr B, element {i}: {arrElement}");
+    }
+}
+
+static void Swap(ref int leftNum, ref int rightNum)
+{
+    var temp = leftNum;
+    leftNum = rightNum;
+    rightNum = temp;
+}
+
+static int[] BubbleSortArr(int[] arrB)
+{
+    for (var j = 0; j < arrB.Length; j++)
+    {
+        for (var i = 0; i < arrB.Length - 1; i++)
+        {
+            if (arrB[i] < arrB[i + 1])
+            {
+                Swap(ref arrB[i], ref arrB[i + 1]);
+            }
+        }
+    }
+    return arrB;
+}
