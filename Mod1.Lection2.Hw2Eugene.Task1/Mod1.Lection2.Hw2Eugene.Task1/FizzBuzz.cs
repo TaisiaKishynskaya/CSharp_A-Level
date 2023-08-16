eb2540a2eb16033a05059992dba1110a0516e835
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-08-16 15:03:02 +0300

    Done task 1 (Eugene)

---
+++ Mod1.Lection2.Hw2Eugene.Task1/Mod1.Lection2.Hw2Eugene.Task1/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+var arr = new int[100];
+
+FillingArray();
+
+void FillingArray() 
+{
+    for (var i = 0; i < arr.Length; i++) 
+    {
+        arr[i] = i + 1;
+
+        if (arr[i] % 3 == 0 && arr[i] % 5 == 0)
+        {
+            Console.WriteLine($"{arr[i] = i + 1} is FizzBuzz");
+        }
+        else if(arr[i] % 5 == 0) 
+        {
+            Console.WriteLine($"{arr[i] = i + 1} is Buzz");
+        } 
+        else if (arr[i] % 3 == 0) 
+        {
+            Console.WriteLine($"{arr[i] = i + 1} is Fizz");
+        }
+        else 
+        {
+            Console.WriteLine($"{arr[i] = i + 1} isn't Fizz/Buzz/FizzBuzz");
+        }
+    }
+}
