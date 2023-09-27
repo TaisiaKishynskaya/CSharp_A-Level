9462aa56d51afb08bf3715ea41271f447f53e149
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-09-27 20:02:34 +0200

    Done hw 1.1

---
+++ Mod3.Lection2.Hw1.1/Mod3.Lection2.Hw1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Mod3.Lection2.Hw1._1;
+
+internal class Program
+{
+    static void Main()
+    {
+        var order = new Post();
+
+        order.MailArrived += Email.Send;
+        order.MailArrived += SMS.Send;
+
+        order.OnMailArrived();
+    }
+}
