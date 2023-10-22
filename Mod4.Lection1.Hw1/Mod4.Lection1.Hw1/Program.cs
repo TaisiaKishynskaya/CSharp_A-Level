93cdee752ed0bf81871ec85e4ec310b19f839a1e
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-10-22 22:13:59 +0300

    Done hw 4 task 1

--- Mod4.Lection1.Hw1/Mod4.Lection1.Hw1/Program.cs
+++ Mod4.Lection1.Hw1/Mod4.Lection1.Hw1/Program.cs
@@ -5,23 +5,23 @@
         internal static async Task Main()
         {
             //await ReqresApi.GetListUsersAsync();
-            //await ReqresApi.GetSingleUserAsync();
-            //await ReqresApi.GetSingleUserNotFound();
-
-            //await ReqresApi.GetListRecourceAsync();
-            //await ReqresApi.GetSingleRecourceAsync();
-            //await ReqresApi.GetSingleRecourceNotFound();
-
-            //await ReqresApi.GetDelayedResponseAsync();
-
+            await ReqresApi.GetSingleUserAsync();
+            await ReqresApi.GetSingleUserNotFound();
+            await ReqresApi.GetListRecourceAsync();
+            await ReqresApi.GetSingleRecourceAsync();
+            await ReqresApi.GetSingleRecourceNotFound();
+            await ReqresApi.GetDelayedResponseAsync();
 
             //await ReqresApi.PostCreateAsync();
+            //await ReqresApi.PostRegisterSuccessfulAsync();
+            //await ReqresApi.PostRegisterUnsuccessfulAsync();
+            //await ReqresApi.PostLoginSuccessfulAsync();
+            //await ReqresApi.PostLoginUnsuccessfulAsync();
 
-            await ReqresApi.PostRegisterSuccessfulAsync();
-            await ReqresApi.PostRegisterUnsuccessfulAsync();
+            //await ReqresApi.PutUpdateAsync();
+            //await ReqresApi.PatchUpdateAsync();
 
-            await ReqresApi.PostLoginSuccessfulAsync();
-            await ReqresApi.PostLoginUnsuccessfulAsync();
+            //await ReqresApi.DeleteInfoAsync();
         }
     }
 }
--- Mod4.Lection1.Hw1/Mod4.Lection1.Hw1/ReqresApi.cs
+++ Mod4.Lection1.Hw1/Mod4.Lection1.Hw1/ReqresApi.cs
@@ -9,17 +9,28 @@ namespace Mod4.Lection1.Hw1;
 internal class ReqresApi
 {
     private const string _reqresURL = "https://reqres.in/";
+    private const string _apiUsers2 = "/api/users/2";
     private const string _apiRegister = "/api/register";
     private const string _apiLogin = "/api/login";
+
+    private const string _contentType = "application/json";
 
     private const string _notFoundMessage = $"\n404 - NotFound Code from reqres site.";
     private const string _badRequestMessage = $"\n400 - BadRequest Code from reqres site.";
     private const string _okMessage = $"\n200 - Ok Code from reqres site.";
 
-    public static async Task ReadAndPrintFromJson(HttpResponseMessage result)
+
+    private static async Task<string> ReadAndPrint(HttpResponseMessage request)
     {
-        var content = await result.Content.ReadAsStringAsync(); // асинхронно считываем содержимое ответa
+        var content = await request.Content.ReadAsStringAsync();
         Console.WriteLine(content);
+
+        return content;
+    }
+
+    public static async Task Deserialization(HttpResponseMessage result)
+    {
+        var content = await ReadAndPrint(result); // асинхронно считываем содержимое ответa
 
         // библиотеку JsonConvert: десериализовать строку JSON (content) в объект типа ReqresPageResponse. JSON-строка преобразуется в экземпляр этого объекта.
         var pageRequest = JsonConvert.DeserializeObject<ReqresPageResponse>(content);
@@ -30,131 +41,8 @@ internal class ReqresApi
         }
     }
 
-    /*public static void SerializationOptions()
+    private static HttpContent Serealization(CreateUserParametersRequest userParametersRequest)
     {
-        // для сериализации объекта userParametersRequest в строку JSON; Опции сериализации задаются в JsonSerializerSettings 
-        var serializedUser = JsonConvert.SerializeObject(userParametersRequest, new JsonSerializerSettings
-        {
-            ContractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new CamelCaseNamingStrategy() // параметр - стиль именования полей (в данном случае, CamelCase)
-            }
-        });
-    }*/
-
-    public static async Task GetListUsersAsync()
-    {
-        using var client = new HttpClient(); // т.к unmanaged (реализует IDispose)
-        // установка базового адреса (все последующие HTTP-запросы, выполненные через клиент, будут относиться к этому базовому URL)
-        client.BaseAddress = new Uri(_reqresURL);
-
-        var result = await client.GetAsync("api/users?page=2"); // https://reqres.in/api/users?page=2
-
-        if (result.StatusCode == HttpStatusCode.OK)
-        {
-            Console.WriteLine(_okMessage);
-
-            await ReadAndPrintFromJson(result);
-        }
-    }
-
-    public static async Task GetSingleUserAsync()
-    {
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(_reqresURL);
-
-        var result = await client.GetAsync("/api/users/2");
-
-        if (result.StatusCode == HttpStatusCode.OK)
-        {
-            Console.WriteLine(_okMessage);
-
-            await ReadAndPrintFromJson(result);
-        }
-    }
-
-    public static async Task GetSingleUserNotFound()
-    {
-        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL)};
-
-        var result = await client.GetAsync("/api/users/23");
-
-        if (result.StatusCode == HttpStatusCode.NotFound)
-        {
-            Console.WriteLine(_notFoundMessage);
-
-            await ReadAndPrintFromJson(result);
-        }
-    }
-
-    public static async Task GetListRecourceAsync()
-    {
-        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
-
-        var result = await client.GetAsync("/api/unknown");
-
-        if (result.StatusCode == HttpStatusCode.OK)
-        {
-            Console.WriteLine(_okMessage);
-
-            await ReadAndPrintFromJson(result);
-        }
-    }
-
-    public static async Task GetSingleRecourceAsync()
-    {
-        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
-
-        var result = await client.GetAsync("/api/unknown/2");
-
-        if (result.StatusCode == HttpStatusCode.OK)
-        {
-            Console.WriteLine(_okMessage);
-
-            await ReadAndPrintFromJson(result);
-        }
-    }
-
-    public static async Task GetSingleRecourceNotFound()
-    {
-        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
-
-        var result = await client.GetAsync("/api/unknown/23");
-
-        if (result.StatusCode == HttpStatusCode.NotFound)
-        {
-            Console.WriteLine(_notFoundMessage);
-
-            await ReadAndPrintFromJson(result);
-        }
-    }
-
-    public static async Task GetDelayedResponseAsync()
-    {
-        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
-
-        var result = await client.GetAsync("/api/users?delay=3");
-
-        if (result.StatusCode == HttpStatusCode.OK)
-        {
-            Console.WriteLine(_okMessage);
-
-            await ReadAndPrintFromJson(result);
-        }
-    }
-
-
-    public static async Task PostCreateAsync()
-    {
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(_reqresURL);
-
-        var userParametersRequest = new CreateUserParametersRequest
-        {
-            Name = "Taisiia",
-            Job = "Student"
-        };
-
         // для сериализации объекта userParametersRequest в строку JSON; Опции сериализации задаются в JsonSerializerSettings 
         var serializedUser = JsonConvert.SerializeObject(userParametersRequest, new JsonSerializerSettings
         {
@@ -165,15 +53,131 @@ internal class ReqresApi
         });
 
         // представляет данные, отправляемые в POST-запросе;  application/json - required (тип контента)
-        var stringContent = new StringContent(serializedUser, Encoding.Unicode, "application/json"); 
+        var stringContent = new StringContent(serializedUser, Encoding.Unicode, _contentType);
+        return stringContent;
+    }
 
-        var result = await client.PostAsync("api/users", stringContent);
 
-        if (result.StatusCode == HttpStatusCode.Created)
+    public static async Task GetListUsersAsync()
+    {
+        using var client = new HttpClient(); // т.к unmanaged (реализует IDispose)
+        // установка базового адреса (все последующие HTTP-запросы, выполненные через клиент, будут относиться к этому базовому URL)
+        client.BaseAddress = new Uri(_reqresURL);
+
+        var response = await client.GetAsync("api/users?page=2"); // https://reqres.in/api/users?page=2
+
+        if (response.StatusCode == HttpStatusCode.OK)
         {
-            Console.WriteLine($"\nStatusCode Created");
+            Console.WriteLine(_okMessage);
 
-            var content = await result.Content.ReadAsStringAsync();
+            await Deserialization(response);
+        }
+    }
+
+    public static async Task GetSingleUserAsync()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
+
+        var response = await client.GetAsync(_apiUsers2);
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine(_okMessage);
+
+            await ReadAndPrint(response);
+        }
+    }
+
+    public static async Task GetSingleUserNotFound()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL)};
+
+        var response = await client.GetAsync("/api/users/23");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            Console.WriteLine(_notFoundMessage);
+
+            await ReadAndPrint(response);
+        }
+    }
+
+    public static async Task GetListRecourceAsync()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
+
+        var response = await client.GetAsync("/api/unknown");
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine(_okMessage);
+
+            await Deserialization(response);
+        }
+    }
+
+    public static async Task GetSingleRecourceAsync()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
+
+        var response = await client.GetAsync("/api/unknown/2");
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine(_okMessage);
+
+            await ReadAndPrint(response);
+        }
+    }
+
+    public static async Task GetSingleRecourceNotFound()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
+
+        var response = await client.GetAsync("/api/unknown/23");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            Console.WriteLine(_notFoundMessage);
+
+            await ReadAndPrint(response);
+        }
+    }
+
+    public static async Task GetDelayedResponseAsync()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
+
+        var response = await client.GetAsync("/api/users?delay=3");
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine(_okMessage);
+
+            await Deserialization(response);
+        }
+    }
+
+
+    public static async Task PostCreateAsync()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
+
+        var userParametersRequest = new CreateUserParametersRequest
+        {
+            Name = "taisiia",
+            Job = "student"
+        };
+
+        var stringContent = Serealization(userParametersRequest);
+
+        var response = await client.PostAsync("api/users", stringContent);
+
+        if (response.StatusCode == HttpStatusCode.Created)
+        {
+            Console.WriteLine($"\n201 - Created Code from reqres site.");
+
+            var content = await response.Content.ReadAsStringAsync();
             Console.WriteLine(content);
 
             var userCreatedResponse = JsonConvert.DeserializeObject<UserCreatedResponse>(content);
@@ -183,8 +187,7 @@ internal class ReqresApi
 
     public static async Task PostRegisterSuccessfulAsync()
     {
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(_reqresURL);
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
 
         var userParametersRequest = new CreateUserParametersRequest
         {
@@ -192,34 +195,21 @@ internal class ReqresApi
             Password = "pistol"
         };
 
-        var serializedUser = JsonConvert.SerializeObject(userParametersRequest, new JsonSerializerSettings
-        {
-            ContractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new CamelCaseNamingStrategy()
-            }
-        });
+        var stringContent = Serealization(userParametersRequest);
 
-        var stringContent = new StringContent(serializedUser, Encoding.Unicode, "application/json");
+        var response = await client.PostAsync(_apiRegister, stringContent);
 
-        var result = await client.PostAsync(_apiRegister, stringContent);
-
-        if (result.StatusCode == HttpStatusCode.OK)
+        if (response.StatusCode == HttpStatusCode.OK)
         {
             Console.WriteLine(_okMessage);
 
-            var content = await result.Content.ReadAsStringAsync();
-            Console.WriteLine(content);
-
-            var userCreatedResponse = JsonConvert.DeserializeObject<UserCreatedResponse>(content);
-            Console.WriteLine(userCreatedResponse!.Name);
+            await ReadAndPrint(response);
         }
     }
 
     public static async Task PostLoginSuccessfulAsync()
     {
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(_reqresURL);
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
 
         var userParametersRequest = new CreateUserParametersRequest
         {
@@ -227,95 +217,136 @@ internal class ReqresApi
             Password = "cityslicka"
         };
 
-        var serializedUser = JsonConvert.SerializeObject(userParametersRequest, new JsonSerializerSettings
-        {
-            ContractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new CamelCaseNamingStrategy()
-            }
-        });
+        var stringContent = Serealization(userParametersRequest);
 
-        var stringContent = new StringContent(serializedUser, Encoding.Unicode, "application/json");
+        var response = await client.PostAsync(_apiLogin, stringContent);
 
-        var result = await client.PostAsync(_apiLogin, stringContent);
-
-        if (result.StatusCode == HttpStatusCode.OK)
+        if (response.StatusCode == HttpStatusCode.OK)
         {
             Console.WriteLine(_okMessage);
 
-            var content = await result.Content.ReadAsStringAsync();
-            Console.WriteLine(content);
-
-            var userCreatedResponse = JsonConvert.DeserializeObject<UserCreatedResponse>(content);
-            Console.WriteLine(userCreatedResponse!.Name);
+            await ReadAndPrint(response);
         }
     }
 
     public static async Task PostRegisterUnsuccessfulAsync()
     {
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(_reqresURL);
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
 
         var userParametersRequest = new CreateUserParametersRequest
         {
             Email = "sydney@fife"
         };
 
-        var serializedUser = JsonConvert.SerializeObject(userParametersRequest, new JsonSerializerSettings
-        {
-            ContractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new CamelCaseNamingStrategy()
-            }
-        });
+        var stringContent = Serealization(userParametersRequest);
 
-        var stringContent = new StringContent(serializedUser, Encoding.Unicode, "application/json");
+        var response = await client.PostAsync(_apiRegister, stringContent);
 
-        var result = await client.PostAsync(_apiRegister, stringContent);
-
-        if (result.StatusCode == HttpStatusCode.BadRequest)
+        if (response.StatusCode == HttpStatusCode.BadRequest)
         {
             Console.WriteLine(_badRequestMessage);
 
-            var content = await result.Content.ReadAsStringAsync();
-            Console.WriteLine(content);
-
-            var userCreatedResponse = JsonConvert.DeserializeObject<UserCreatedResponse>(content);
-            Console.WriteLine(userCreatedResponse!.Name);
+            await ReadAndPrint(response);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode}");
         }
     }
 
     public static async Task PostLoginUnsuccessfulAsync()
     {
-        using var client = new HttpClient();
-        client.BaseAddress = new Uri(_reqresURL);
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
 
         var userParametersRequest = new CreateUserParametersRequest
         {
             Email = "peter@klaven"
         };
 
-        var serializedUser = JsonConvert.SerializeObject(userParametersRequest, new JsonSerializerSettings
-        {
-            ContractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new CamelCaseNamingStrategy()
-            }
-        });
+        var stringContent = Serealization(userParametersRequest);
 
-        var stringContent = new StringContent(serializedUser, Encoding.Unicode, "application/json");
+        var response = await client.PostAsync(_apiLogin, stringContent);
 
-        var result = await client.PostAsync(_apiLogin, stringContent);
-
-        if (result.StatusCode == HttpStatusCode.BadRequest)
+        if (response.StatusCode == HttpStatusCode.BadRequest)
         {
             Console.WriteLine(_badRequestMessage);
 
-            var content = await result.Content.ReadAsStringAsync();
-            Console.WriteLine(content);
+            await ReadAndPrint(response);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode}");
+        }
+    }
 
-            var userCreatedResponse = JsonConvert.DeserializeObject<UserCreatedResponse>(content);
-            Console.WriteLine(userCreatedResponse!.Name);
+
+    public static async Task PutUpdateAsync()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
+
+        var updatedData = new CreateUserParametersRequest // Заменить на данные, которые хотим отправить
+        {
+            Name = "morpheus",
+            Job = "zion resident"
+        };
+
+        var stringContent = Serealization(updatedData);
+
+        var response = await client.PutAsync(_apiUsers2, stringContent);
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine($"{_okMessage}. Resource updated successfully.");
+
+            await ReadAndPrint(response);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode}");
+        }
+    }
+
+    public static async Task PatchUpdateAsync()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
+
+        var updatedData = new CreateUserParametersRequest // Заменить на данные, которые хотим отправить
+        {
+            Name = "morpheus",
+            Job = "zion resident"
+        };
+
+        var stringContent = Serealization(updatedData);
+
+        var response = await client.PatchAsync(_apiUsers2, stringContent);
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine($"{_okMessage}. Resource updated successfully.");
+
+            await ReadAndPrint(response);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode}");
+        }
+    }
+
+    public static async Task DeleteInfoAsync()
+    {
+        using var client = new HttpClient { BaseAddress = new Uri(_reqresURL) };
+
+        var response = await client.DeleteAsync(_apiUsers2);
+
+        if (response.StatusCode == HttpStatusCode.NoContent)
+        {
+            Console.WriteLine($"\n204 - NoContent Code from reqres site.");
+
+            await ReadAndPrint(response);
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode}");
         }
     }
 }
