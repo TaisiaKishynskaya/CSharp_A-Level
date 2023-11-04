24cce974bddeabfb94257469ac5cbb37f6f75001
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-11-04 17:01:25 +0200

    Step 4-5

--- Mod4.Lection4.Hw/Mod4.Lection4.Hw/Context/EFCoreContext.cs
+++ Mod4.Lection4.Hw/Mod4.Lection4.Hw/Context/EFCoreContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Mod4.Lection4.Hw.Models;
 
 namespace Mod4.Lection4.Hw.Context;
 
@@ -7,5 +9,23 @@ public class EFCoreContext : DbContext
     // DbContext не має знати, де він буде використовуватись, тому тут не повинно бути конфігурації.
     // DbContext має описувати, що він вміє підключатись з якимись налаштуваннями
 
+    // Fkuent API
+
+    public DbSet<User> Users { get; private set; }  // щоб EF почав працювати з моделью з коду до бд
+
     public EFCoreContext(DbContextOptions<EFCoreContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(x => x.Id);  // вказуємо, що є ключем
+        modelBuilder.Entity<User>().Property(b => b.Name).IsRequired();  // не null
+        modelBuilder.Entity<User>().Property(b => b.Surname).IsRequired();
+
+        // конвертація типа, кастомний конвертор
+        modelBuilder.Entity<User>().Property(x => x.Birthday)
+            .HasConversion(new ValueConverter<DateOnly?, DateTime?>(
+                x => x.HasValue ? x.Value.ToDateTime(TimeOnly.MinValue) : null,
+                y => y.HasValue ? DateOnly.FromDateTime(y.Value) : null)
+            );
+    }
 }
---
+++ Mod4.Lection4.Hw/Mod4.Lection4.Hw/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Mod4.Lection4.Hw.Models;
+
+public class User // назва класу - назва таблиці
+{
+    // назва св-ва - назва колонки
+
+    public int Id { get; set; }  // всі св-ва, що мають в назві id, за конвенцією будуть використані як первинний та вторинний ключі
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public DateOnly? Birthday { get; set; }  // DateOnly не існіє в скл, тому так воно не запрацює, двитись в конвенціях
+}
