22d2fe6ccf617a5d022f0bc1c16e651bc970f1f6
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-11-04 23:43:02 +0200

    Step 7

---
+++ Mod4.Lection4.Hw/Mod4.Lection4.Hw/Context/Configurations/UserSettingConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Mod4.Lection4.Hw.Models;
+
+namespace Mod4.Lection4.Hw.Context.Configurations;
+
+public class UserSettingConfiguration : IEntityTypeConfiguration<UserSettings>
+{
+    public void Configure(EntityTypeBuilder<UserSettings> builder)
+    {
+        builder
+            .Property(x => x.Theme)
+            .HasConversion(
+                v => v.ToString(),
+                v => (ThemeSetting)Enum.Parse(typeof(ThemeSetting), v)
+            );
+        builder
+            .HasOne(x => x.User)
+            .WithOne(x => x.UserSettings)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
--- Mod4.Lection4.Hw/Mod4.Lection4.Hw/Context/EFCoreContext.cs
+++ Mod4.Lection4.Hw/Mod4.Lection4.Hw/Context/EFCoreContext.cs
@@ -18,43 +18,10 @@ public class EFCoreContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>().HasKey(x => x.Id);  // вказуємо, що є ключем
-        modelBuilder.Entity<User>().Property(b => b.Name).IsRequired();  // не null
-        modelBuilder.Entity<User>().Property(b => b.Surname).IsRequired();
+        // Option 1
+        //modelBuilder.ApplyConfiguration(new UserConfiguration());
 
-        // конвертація типа, кастомний конвертор
-        modelBuilder.Entity<User>().Property(x => x.Birthday)
-            .HasConversion(new ValueConverter<DateOnly?, DateTime?>(
-                x => x.HasValue ? x.Value.ToDateTime(TimeOnly.MinValue) : null,
-                y => y.HasValue ? DateOnly.FromDateTime(y.Value) : null)
-            );
-
-        
-        modelBuilder.Entity<User>()
-            .HasOne(x => x.UserSettings) // в юзера є один UserSettings
-            .WithOne(x => x.User)  // де є 1 користувач
-            .OnDelete(DeleteBehavior.ClientSetNull); // поведінка дляя форінг ключей (тільки на апдейт або видалення) - коли видаляєв налаштування, користувач не видалиться
-
-        modelBuilder.Entity<User>().HasMany(x => x.Articles).WithMany(x => x.Athors);
-        modelBuilder.Entity<User>().HasMany(x => x.BlogSubscribsions).WithMany(x => x.Readers);
-
-        // ------------- User settings
-        modelBuilder.Entity<UserSettings>().Property(x => x.Theme)
-            .HasConversion(
-                v => v.ToString(),
-                v => (ThemeSetting)Enum.Parse(typeof(ThemeSetting), v)
-            );
-        modelBuilder.Entity<UserSettings>()
-            .HasOne(x => x.User)
-            .WithOne(x => x.UserSettings)
-            .OnDelete(DeleteBehavior.Cascade);
-
-        // ------------- Blog
-        modelBuilder.Entity<Blog>().HasMany(x => x.Articles).WithOne(x => x.Blog); // багато статей мають 1 блог
-        modelBuilder.Entity<Blog>().HasMany(x => x.Readers).WithMany(x => x.BlogSubscribsions); // багато до багатьох
-
-        // ------------- Article
-        modelBuilder.Entity<Article>().HasOne(x => x.Blog).WithMany(x => x.Articles);  // 1 блог має багато статей
-        modelBuilder.Entity<Article>().HasMany(x => x.Athors).WithMany(x => x.Articles);
+        // Option 2 - працює через рефлексію, треба знайти тип проекту і взяли в нього асемблі, шукає всі реалізації IEntityTypeConfiguration
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
     }
 }
