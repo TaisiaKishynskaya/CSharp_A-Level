5812b4ae390392567fb1dc1a76940960d93918f1
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-11-04 16:25:45 +0200

    Step 3

--- Mod4.Lection4.Hw/Mod4.Lection4.Hw/Context/EFCoreContext.cs
+++ Mod4.Lection4.Hw/Mod4.Lection4.Hw/Context/EFCoreContext.cs
@@ -7,6 +7,5 @@ public class EFCoreContext : DbContext
     // DbContext не має знати, де він буде використовуватись, тому тут не повинно бути конфігурації.
     // DbContext має описувати, що він вміє підключатись з якимись налаштуваннями
 
-    public EFCoreContext() { }
     public EFCoreContext(DbContextOptions<EFCoreContext> options) : base(options) { }
 }
