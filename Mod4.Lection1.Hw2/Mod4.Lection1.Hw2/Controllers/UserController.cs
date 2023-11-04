8e7cfe23822370fe4533bfa89cd1463da4bebdb6
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-11-04 16:09:08 +0200

    Step 1

---
+++ Mod4.Lection1.Hw2/Mod4.Lection1.Hw2/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mod4.Lection1.Hw2.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    
+}
---
+++ Mod4.Lection2.Hw1/Mod4.Lection2.Hw1/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Mod4.Lection2.Hw1
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
