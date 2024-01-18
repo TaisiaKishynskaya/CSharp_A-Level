297584a4cae84cd3172297de2ed4a4306c246397
Taisia <113723880+TaisiaKishynskaya@users.noreply.github.com>
2024-01-18 20:29:42 +0200

    Add RateLimit

---
+++ eShop/Infrastructure/Infrastructure.RateLimit/RateLimitMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using StackExchange.Redis;
+
+namespace Infrastructure.RateLimit;
+
+public class RateLimitMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IDatabase _redis;
+
+    public RateLimitMiddleware(RequestDelegate next, IConnectionMultiplexer redis)
+    {
+        _next = next;
+        _redis = redis.GetDatabase();
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var ip = context.Connection.RemoteIpAddress.ToString();
+        var endpoint = context.Request.Path;
+
+        var key = $"{ip}:{endpoint}";
+
+        var currentRequestCount = await _redis.StringIncrementAsync(key, 1);
+
+        if (currentRequestCount == 1)
+        {
+            await _redis.KeyExpireAsync(key, TimeSpan.FromMinutes(1));
+        }
+
+        if (currentRequestCount > 10)
+        {
+            context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+            return;
+        }
+
+        await _next(context);
+    }
+}
